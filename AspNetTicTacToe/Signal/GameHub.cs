7a7cdaee60a3b5f2ec616369e5513b6da8eae5b8
Katuha-ha <katy_04_94@mail.ru>
2021-08-05 23:46:28 +0300

    Т.к. целевые платформы WPF и библиотеки игры расходятся, принято решение реализовать взаимодействие через SignalR

---
+++ AspNetTicTacToe/Signal/GameHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetTicTacToe.Signal
+{
+    public class GameHub:Hub
+    {
+        public async Task Test()
+        {
+
+        }
+    }
+}
