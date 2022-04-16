29f5c2335f99e68919fb7b0346f7ae94bd65de7a
Gosha94 <gerizch@rambler.ru>
2022-04-17 00:42:12 +0300

    add settings to project

---
+++ MatchMake.Backend/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MatchMake.Backend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UsePostgreSqlStorage("SomeConnString"));
+            services.AddHangfireServer();            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHangfireDashboard("/hangfire_board");
+        }
+    }
+}
