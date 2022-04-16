8de9a3a5e61ba1d6d40f0cf619f36da9742d03d8
Gosha94 <gerizch@rambler.ru>
2022-04-17 00:42:12 +0300

    add settings to project

---
+++ MatchMake.Backend/Domain/Processes/UserNotification_TestHangfire/NotificationProcess.cs
@@ -0,0 +1,53 @@
+﻿using Hangfire;
+
+namespace MatchMake.Backend.Domain.Processes.UserNotification_TestHangfire
+{
+    public class NotificationProcess
+    {
+        
+        public void FireAndForgetJobStart()
+        {
+            string jobId = BackgroundJob.Enqueue(
+                () => Console.WriteLine("Fire And Forget Job is Started")
+                );
+
+            Console.WriteLine($"Fire And Forget Job Id = {jobId}");
+        }
+
+        public void DelayedJobStart()
+        {
+            string jobId = BackgroundJob.Schedule(
+                () => Console.WriteLine("Delayed Scheduling Job is started"),
+                TimeSpan.FromSeconds(20)
+                );
+            
+            Console.WriteLine($"Delayed Job Id = {jobId}");
+        }
+
+        /// <summary>
+        /// Повторяющаяся задача
+        /// </summary>
+        public void RecurringJobStart()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Здесь будет ваше поздравление с ДР"), Cron.Daily);
+
+            Console.WriteLine($"Recurring Job Is Planning");
+        }
+
+        /// <summary>
+        /// Продолжающаяся задача
+        /// </summary>
+        public void ContinuationJobStart()
+        {
+            string jobId = BackgroundJob.Enqueue(()=>
+                Console.WriteLine("Основная задача")
+            );
+
+            BackgroundJob.ContinueJobWith(jobId, () =>
+                Console.WriteLine($"Продолжение задачи с jobId - {jobId} (Основная задача)")
+            );
+
+        }
+
+    }
+}
--- MatchMake.Backend/Program.cs
+++ MatchMake.Backend/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, MatchMake.Backend!");
+﻿using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, MatchMake.Backend!");
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//Configuration = new ConfigurationBuilder()
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json")
+//    .AddJsonFile($"appsettings.{env.Name}.json")
+//    .AddJsonFile("secrets.json")    
+//    .AddEnvironmentVariables()
+//    .Build();
+
+// Hangfire
+// https://www.youtube.com/watch?v=iilRdmNILC8
+// https://github.com/codaza/Hangfire
