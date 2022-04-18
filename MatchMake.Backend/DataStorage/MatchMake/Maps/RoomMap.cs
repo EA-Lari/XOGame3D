9ced6d72f5ebfc19f0c68b6f6333b0b86b9057b8
Gosha94 <gerizch@rambler.ru>
2022-04-18 18:32:48 +0300

    start adding repos and mapping to tables

---
+++ MatchMake.Backend/DataStorage/MatchMake/Maps/RoomMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static MatchMake.Backend.Storage.DbaseMapping.RoomMap;
+
+namespace MatchMake.Backend.Storage.DbaseMapping
+{
+
+    public class RoomMap : IEntityTypeConfiguration<SomeExampleContext>
+    {
+        public void Configure(EntityTypeBuilder<SomeExampleContext> builder)
+        {
+            builder.ToTable("tRooms", "matchmake_backend");
+            builder.HasKey(p => p.Id);
+        }
+
+        public class SomeExampleContext : DbContext {
+            public int Id { get; set; }
+        }
+
+    }
+}
