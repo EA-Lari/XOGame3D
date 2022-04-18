a7b1eff6cbcd9057e2d3b26304deabcae41922d4
Gosha94 <gerizch@rambler.ru>
2022-04-18 18:32:48 +0300

    start adding repos and mapping to tables

---
+++ MatchMake.Backend/DataStorage/MatchMake/MatchMakeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MatchMake.Backend.DataStorage.MatchMake.Context
+{
+    public class MatchMakeContext : DbContext
+    {
+        
+        public MatchMakeContext(DbContextOptions<MatchMakeContext> options) : base(options)
+        {   }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration();
+            modelBuilder.ApplyConfiguration();
+        }
+
+    }
+}
