57d8ccb1f3fce381c68e04472929bb1e812c2cc1
Gosha94 <gerizch@rambler.ru>
2022-04-23 01:10:01 +0300

    hangfire job is works!

--- MatchMake.Backend/DomainLogic/Processes/HelloWorldTestProcess.cs
+++ MatchMake.Backend/DomainLogic/Processes/HelloWorldTestProcess.cs
@@ -1,13 +1,11 @@
-﻿using System.Diagnostics;
-using MatchMake.Backend.Contracts;
-using Microsoft.Extensions.Logging;
+﻿using MatchMake.Backend.Contracts;
 
 namespace MatchMake.Backend.Processes
 {
     public class HelloWorldTestProcess : IParallelProcess
     {
 
-        private Timer _timer;
+        //private Timer _timer;
         //private readonly ILogger _logger;
 
         public HelloWorldTestProcess()
@@ -17,18 +15,20 @@ namespace MatchMake.Backend.Processes
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _timer = new Timer(HelloWorld, null, 0, 10000);
+            //_timer = new Timer(HelloWorld, null, 0, 10000);
+            //return Task.CompletedTask;
+            HelloWorld();
             return Task.CompletedTask;
         }
 
-        private void HelloWorld(object state)
+        private void HelloWorld()
         {
             Console.WriteLine("HelloWorldProcess awake!");
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _timer?.Change(Timeout.Infinite, 0);
+            //_timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
         }
     }
--- MatchMake.Backend/Program.cs
+++ MatchMake.Backend/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSingleton<IParallelProcess, HelloWorldTestProcess>();
 
 builder.Services.AddHangfire(configuration =>
                     {
-                        configuration.UsePostgreSqlStorage("Host=localhost;Port=30000;Database=xo_admin;User Id=xo_admin;Password=xo_admin;", new PostgreSqlStorageOptions() { SchemaName = "matchmake_jobs"});
+                        configuration.UsePostgreSqlStorage("Host=localhost;Port=25432;Database=xo_admin;User Id=xo_admin;Password=xo_admin;", new PostgreSqlStorageOptions() { SchemaName = "matchmake_jobs"});
                     });
 
 builder.Services.AddHangfireServer();
@@ -38,73 +38,14 @@ lifetime.ApplicationStarted.Register(() =>
 app.Logger.LogInformation("Hi! The MatchMake.Backend is Running!");
 app.UseHangfireDashboard("/jobs");
 
+#region Scheduling Jobs
+
+RecurringJob.AddOrUpdate<IParallelProcess>("HelloWorldTestProcess", x => x.StartAsync(new CancellationToken()), Cron.Minutely);
+
+//BackgroundJob.Enqueue(
+//                () => Console.WriteLine("Test Job")
+//                );
+
+#endregion
+
 app.Run();
-
-Task.Delay(5000);
-
-BackgroundJob.Enqueue(
-                () => process.StartAsync(new CancellationToken())
-                );
-
-//CreateHostBuilder(args).Build().Run();
-
-
-//static IHostBuilder CreateHostBuilder(string[] args) =>
-//            Host.CreateDefaultBuilder(args)
-//                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-//                .ConfigureContainer<ContainerBuilder>(ConfigureContainer)
-//                .ConfigureServices(services => services.Configure<ConsoleLifetimeOptions>(options => options.SuppressStatusMessages = true))
-//                .ConfigureServices((hostContext, services) =>
-//                {
-//                    //services.AddHostedService<StartHostProcessesService>();
-//                });
-
-//static void ConfigureContainer(HostBuilderContext hostContext, ContainerBuilder builder)
-//{
-//    builder.RegisterType<StartHostProcessesService>().As<IHostedService>().SingleInstance();
-
-//    var Config = new Settings();
-//    SettingsExtensions.SetFromEnvironmentVariables(Config);
-
-//    #region Logger
-
-//    var logger = Log.Logger;
-//    logger = new LoggerConfiguration().CreateLogger();
-
-//    logger.Information("Стартуем логирование MatchMake.Backend!");
-
-//    builder.Register(context => logger).As<ILogger>().SingleInstance();
-
-//    #endregion
-
-//    builder.Register(context => Config).As<Settings>().SingleInstance();
-
-//    #region Database
-
-//    builder.Register(context =>
-//                    new DbContextOptionsBuilder<MatchMakeContext>()
-//                        .UseNpgsql(Config.ConnectionStrings.MatchMakeContext)
-//                        .Options)
-//                .As<DbContextOptions<MatchMakeContext>>().SingleInstance();
-
-//    builder.RegisterType<MatchMakeContext>()
-//           .InstancePerDependency();
-
-//    builder.RegisterGeneric(typeof(AsyncRepositoryUnderMatchMakeDbase<,>)).As(typeof(IAsyncRepository<,>))
-//           .InstancePerDependency();
-
-//    #endregion
-
-//    #region Automapper Models
-
-//    //builder.Register(ctx => new MapperConfiguration(cfg => { cfg.AddProfile(new Mobile.BackEcosystem.Automapper.MappingProfile(Convert.ToBoolean(Config.IsInstanceForMkb))); }
-//    //));
-//    //builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>();
-
-//    #endregion
-
-//    #region Message Broker
-
-//    #endregion
-
-//}
