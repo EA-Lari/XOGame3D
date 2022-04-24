b18e0c8b3756a135b28f93650ae31f1f2e53689b
Gosha94 <gerizch@rambler.ru>
2022-04-24 15:24:08 +0300

    Add solution

--- GameStreamer.Backend/Program.cs
+++ GameStreamer.Backend/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GameStreamer.Backend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<RoomsHub>("/room");
+app.Run();
