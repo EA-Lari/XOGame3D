174e8cc7178be8c49c1c7be8619ea067fe0e2934
Gosha94 <gerizch@rambler.ru>
2022-05-30 21:11:35 +0300

    add to compose angular client

---
+++ GameStreamer.Backend/Hubs/RoomsHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GameStreamer.Backend.Hubs
+{
+    public class RoomsHub : Hub
+    {
+        public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                yield return DateTime.UtcNow;
+                await Task.Delay(3000, cancellationToken);
+            }
+        }
+
+        public async Task SendHelloWorld(string nickName)
+        {
+            await Clients.All.SendAsync($"Hello from SignalR, {nickName}!");
+        }
+    }
+}
