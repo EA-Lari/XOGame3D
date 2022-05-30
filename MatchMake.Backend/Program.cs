2df5d7d7e78f184080a75a829c89dffacbee4a1c
Gosha94 <gerizch@rambler.ru>
2022-05-30 21:11:35 +0300

    add to compose angular client

--- MatchMake.Backend/Program.cs
+++ MatchMake.Backend/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:MatchMake.Backend/Program.cs
 ﻿using Autofac;
 using Hangfire;
 using Hangfire.PostgreSql;
@@ -110,5 +111,17 @@ var starter = app.Services.GetRequiredService<IProcessStarter>();
 starter.ScheduleAllProcesses();
 
 #endregion
+=======
+﻿using GameStreamer.Backend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<RoomsHub>("/xo-rooms");
+app.MapHub<GamesHub>("/xo-games");
+>>>>>>> Stashed changes:GameStreamer.Backend/Program.cs
 
 app.Run();
