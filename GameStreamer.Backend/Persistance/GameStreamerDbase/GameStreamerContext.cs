ec258ce13808c13ff720f271b72db57e072648c6
Georgy <gerizch@rambler.ru>
2022-06-18 01:19:54 +0300

    add folders

---
+++ GameStreamer.Backend/Persistance/GameStreamerDbase/GameStreamerContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStreamer.Backend.Persistance.GameStreamerDbase
+{
+    public class GameStreamerContext : DbContext
+    {
+        public GameStreamerContext(DbContextOptions<GameStreamerContext> options) : base(options)
+        {   }
+
+        // TODO: Add DbSets Here
+
+        /// <summary>
+        /// Настройка свойств моделей
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasDefaultSchema("dashboard");
+
+            //modelBuilder.ApplyConfiguration(new QuarterReportEntityMap());
+            //modelBuilder.ApplyConfiguration(new ClientsReportEntityMap());
+            //modelBuilder.ApplyConfiguration(new ContractsReportEntityMap());
+        }
+
+    }
+}
