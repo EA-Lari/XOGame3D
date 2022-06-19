8e6818a6e44744079d01c9d0f8a2831088aff20a
Gosha94 <gerizch@rambler.ru>
2022-06-20 00:39:38 +0300

    add persistance arch and entities

---
+++ GameStreamer.Backend/Persistance/GameStreamerDbase/Entities/RoomEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStreamer.Backend.Persistance.GameStreamerDbase.Entities
+{
+    public class RoomEntity
+    {
+        public int Id { get; set; }
+
+        public Guid RoomGuid { get; set; }
+
+        public List<PlayerEntity> Players { get; set; }
+
+    }
+}
