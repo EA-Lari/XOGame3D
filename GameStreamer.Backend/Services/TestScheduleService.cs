ce8e9b86cfa5b89ccdb75ace5e31b4055cebff48
Georgy <gerizch@rambler.ru>
2022-11-30 10:31:43 +0300

    change js scripts

--- GameStreamer.Backend/Services/TestScheduleService.cs
+++ GameStreamer.Backend/Services/TestScheduleService.cs
@@ -1,6 +1,7 @@
 ﻿using GameStreamer.Backend.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using GameStreamer.Backend.Interfaces;
+using GameStreamer.Backend.DTOs;
 
 namespace GameStreamer.Backend.Services
 {
@@ -8,21 +9,35 @@ namespace GameStreamer.Backend.Services
     {
         
         private readonly IHubContext<GameHub, IGameHub> _gameHub;
+        private readonly IHubContext<RoomsHub, IRoomsHub> _roomsHub;
+        private readonly Random _random = new Random();
 
-        public TestScheduleService(IHubContext<GameHub, IGameHub> gameHub)
+
+        public TestScheduleService(IHubContext<GameHub, IGameHub> gameHub, IHubContext<RoomsHub, IRoomsHub> roomsHub)
         {
             _gameHub = gameHub;
+            _roomsHub = roomsHub;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var timer = new PeriodicTimer(TimeSpan.FromMinutes(10));
+            var timer = new PeriodicTimer(TimeSpan.FromSeconds(20));
 
             while (await timer.WaitForNextTickAsync(stoppingToken))
             {
                 Console.WriteLine("TestScheduleService have got next tick! Waiting for 5 sec...");
                 
-                await _gameHub.Clients.All.TestBroadcastPublish("You are pidrila!");
+                await _roomsHub.Clients.All.NewRoomAdded(
+                    new GameRoomResponseDTO { 
+                        RoomName = $"Room_{_random.Next(8000, 15000)}",
+                        PlayersList = new List<PlayerDataResponseDTO> {
+                            new PlayerDataResponseDTO { ConnectionId = "123", NickName = "Pidor_666" },
+                            new PlayerDataResponseDTO { ConnectionId = "456", NickName = "Pidor_777" },
+                        }
+                    });
+
+                //await _gameHub.Clients.All.TestBroadcastPublish("You are pidrila!");
+                
             }
         }
     }
--- GameStreamer.Frontend/js/signalr-test-connector.js
+++ GameStreamer.Frontend/js/signalr-test-connector.js
@@ -5,7 +5,15 @@ const LOBBY_HUB = "/lobbies";
 
 /** Live collections */
 const messagesItems = document.querySelector(".messages-list");
-const messageTemplate = document.getElementById("chat-message-template");
+const roomsItems = document.querySelector(".room-list");
+
+// const roomsLiveCollection = new Map();
+// const playersLiveCollection = new Map();
+
+/** Templates */
+// const chatMessageTemplate = document.getElementById("chat-message-template");
+const gameRoomTemplate = document.getElementById("game-room-template").content;
+const playerTemplate = document.getElementById("player-item-template").content;
 
 /** App EntryPoint */
 
@@ -23,10 +31,24 @@ startHubAsync(lobbyHubConnection);
 const nicknameButton = document.querySelector(".player-nickname-button");
 const nicknameInput = document.querySelector(".player-nickname-input");
 
-nicknameButton.onclick = function() {
+nicknameButton.onclick = function () {
     lobbyHubConnection.invoke("PlayerAddedLogin", nicknameInput.value);
     nicknameButton.disabled = true;
     nicknameInput.disabled = true;
+};
+
+const dropRoomButton = document.querySelector(".drop-room-button");
+const roomNameInput = document.querySelector(".room-name-input");
+
+dropRoomButton.onclick = function () {
+    // roomsLiveCollection.delete();
+    
+    if (roomNameInput.value) {
+        const liForDelete = document.querySelector("[data-roomId*="+ roomNameInput.value +"]");
+        if (liForDelete) {
+            liForDelete.parentElement.removeChild(liForDelete);
+        }
+    }
 };
 
 /** Functions */
@@ -55,13 +77,52 @@ function setupLobbyConnection(lobbyConnection) {
     });
 
     lobbyConnection.on("PlayerLeavedServer", (playerDto) => {
-        
+        console.log(playerDto.nickName + " with conn id: " + playerDto.connectionId + " left from The Game Server!");
+    });
+
+    lobbyConnection.on("NewRoomAdded", (newRoomDto) => {
+        console.log("NewGameRoom is added: " + newRoomDto.roomName);
+
+        var newRoomElement = renderNewRoomElement(newRoomDto);
+        roomsItems.appendChild(newRoomElement);
+
+        // updateRoomsCollection(newRoomDto.roomName, newRoomElement);
     });
 
     lobbyConnection.onclose(async () => {
         // await startHubAsync(lobbyConnection);
         setTimeout(await startHubAsync(lobbyConnection), 5000);
     });
+};
+
+function updatePlayersCollection(playerName, playerElement) {
+    // playersLiveCollection.set(playerName, playerElement);
+};
+
+function renderNewRoomElement(gameRoomDto) {
+    const newRoomItem = gameRoomTemplate.cloneNode(true);
+    const roomNode = newRoomItem.querySelector('.room-item');
+    const roomName = newRoomItem.querySelector('.room-header');
+    var textContent = document.createTextNode(gameRoomDto.roomName);
+    roomNode.setAttribute('data-roomId', gameRoomDto.roomName);
+
+    roomName.appendChild(textContent);
+    const playerList = newRoomItem.querySelector('.player-list');
+
+    gameRoomDto.playersList.forEach((playerDto) => {
+        const newPlayerItem = playerTemplate.cloneNode(true);
+        const playerName = newPlayerItem.querySelector('.player-nickname');
+        var textContent = document.createTextNode(playerDto.nickName);
+        playerName.appendChild(textContent);
+
+        playerList.appendChild(newPlayerItem);
+    });
+
+    return newRoomItem;
+};
+
+function updateRoomsCollection(roomName, htmlRoomElement) {
+    // roomsLiveCollection.set(roomName, htmlRoomElement);
 };
 
 async function startHubAsync(hubConnection) {
